d57768554ab3a45125965a740363eef5c8d05d1e
ahmetugur91 <carnack54@gmail.com>
2019-05-21 14:03:13 +0300

    butonlara zoom efekti eklendi

--- js/sound.js
+++ js/sound.js
@@ -1,9 +1,27 @@
 function playSound(el, soundfile) {
     var element = el;
+
+    $(element).find("img").animate({ 'zoom': 1.2}, 400);
+    $(element).find("img").addClass("redCircle");
+
+    element.mp3 = new Audio(soundfile);
+    element.mp3.onended = function() {
+        $(element).find("img").animate({ 'zoom': 1 }, 400);
+        $(element).find("img").removeClass("redCircle");
+    };
+
+    element.mp3.play();
+
+
+    /*
     if (element.mp3) {
         element.mp3.play();
     } else {
-        element.mp3 = new Audio(soundfile);
-        element.mp3.play();
+
+
     }
+     */
+
+
+
 }
