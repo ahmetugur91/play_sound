e8a4701eee6bac26ae951e459f02ba2c68cef702
ahmetugur91 <carnack54@gmail.com>
2019-05-21 15:12:22 +0300

    disabled buttons and effects

--- js/sound.js
+++ js/sound.js
@@ -1,17 +1,24 @@
+let isPlaying = false;
 function playSound(el, soundfile) {
+
+    if(isPlaying) return false;
+
     var element = el;
 
     $(element).find("img").animate({ 'zoom': 1.2}, 400);
     $(element).find("img").addClass("redCircle");
+    $(".buttons").find("img").not($(el).find("img")).addClass("blur");
 
     element.mp3 = new Audio(soundfile);
     element.mp3.onended = function() {
         $(element).find("img").animate({ 'zoom': 1 }, 400);
         $(element).find("img").removeClass("redCircle");
+        $(".buttons").find("img").removeClass("blur");
+        isPlaying = false;
     };
 
     element.mp3.play();
-
+    isPlaying = true;
 
     /*
     if (element.mp3) {
@@ -21,7 +28,5 @@ function playSound(el, soundfile) {
 
     }
      */
-
-
 
 }
