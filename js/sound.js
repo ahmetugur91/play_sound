2f7294c9686c56e230167253219fcab3b9233d05
ahmetugur91 <carnack54@gmail.com>
2019-06-04 02:21:50 +0300

    aynı anda farklı butonlara basma eklendi

--- js/sound.js
+++ js/sound.js
@@ -1,24 +1,40 @@
 let isPlaying = false;
+let playing_count = 0;
+
 function playSound(el, soundfile) {
 
-    if(isPlaying) return false;
+   // if(isPlaying) return false;
 
     var element = el;
 
-    //$(element).find("img").animate({ 'zoom': 1.2}, 400);
+    $(element).find("img").removeClass("blur");
+
+
     $(element).find("img").addClass("redCircle");
-    $(".buttons").find("img").not($(el).find("img")).addClass("blur");
+    $(".buttons").find("img")
+        .not($(el).find("img"))
+        .not($(".redCircle"))
+        .addClass("blur");
 
     element.mp3 = new Audio(soundfile);
+
     element.mp3.onended = function() {
-        //$(element).find("img").animate({ 'zoom': 1 }, 400);
         $(element).find("img").removeClass("redCircle");
-        $(".buttons").find("img").removeClass("blur");
+
+        if(playing_count <= 1) {
+            $(".buttons").find("img").removeClass("blur");
+        } else {
+            $(element).find("img").addClass("blur");
+        }
+
         isPlaying = false;
+        playing_count = playing_count - 1;
     };
 
     element.mp3.play();
+
     isPlaying = true;
+    playing_count = playing_count + 1;
 
     /*
     if (element.mp3) {
